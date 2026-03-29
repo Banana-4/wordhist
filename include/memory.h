b6ba4f3288a01f50c8d3dba70bb61152e1c01acb
Banana-4 <stig34768@gmail.com>
2026-03-29 20:36:24 +0000

    add StrArray

--- include/memory.h
+++ include/memory.h
@@ -14,10 +14,19 @@ typedef struct {
     char *block;
 }Str;
 
+typedef struct {
+    size_t len;
+    size_t size;
+    Str **block;
+} StrArray;
+
 Array *new_Array();
 Str *new_Str();
+StrArray *new_StrArray();
 
 bool append_Array(Array *a, int n);
 bool append_Str(Str *s, char c);
+bool append_StrArray(StrArray *strs, Str *s);
 void del_Array(Array *a);
 void del_Str(Str *s);
+void del_StrArray(StrArray *str);
