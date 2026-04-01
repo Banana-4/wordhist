8017ec3fe78e344ccc27b1271671bd8fc3a657a7
Banana-4 <stig34768@gmail.com>
2026-04-01 12:42:45 +0000

    implement hash map

---
+++ src/hashmap.c
@@ -0,0 +1,119 @@
+#include "../include/memory.h"
+#include <stdlib.h>
+#include <string.h>
+
+HashMap *new_HashMap() {
+    HashMap *m = (HashMap *)malloc(sizeof(HashMap));
+    if (!m) {
+        return NULL;
+    }
+    m->size = SIZE;
+    m->strs = (StrArray**)malloc(sizeof(StrArray*) * m->size);
+    if (!m->strs) {
+        free(m);
+        return NULL;
+    }
+    for (int i = 0; i < m->size; ++i) {
+        m->strs = NULL;
+    }
+    m->len = 0;
+    return m;
+}
+
+
+
+//AI generated hash fucntion
+unsigned long __hash(const char *str) {
+    unsigned long hash = 5381;
+    int c;
+
+    while ((c = *str++)) {
+        hash = ((hash << 5) + hash) + c;
+        // hash * 33 + c
+    }
+    return hash;
+}
+
+int __hash_Str(Str *s, size_t range) {
+  return __hash(s->block) % range;
+}
+
+bool __grow_HashMap(HashMap *m) {
+    StrArray **new = realloc(m->strs, sizeof(StrArray *) * m->size * 2);
+    if (!new) {
+        return false;
+    }
+    for (int i = 0; i < m->size * 2; ++i)
+        new[i] = NULL;
+    size_t oldSize = m->size;
+    StrArray **oldStrs = m->strs;
+    m->size = m->size * 2;
+    m->strs = new;
+
+    for (int i = 0; i < oldSize; ++i) {
+        StrArray *bucket = oldStrs[i];
+        if (bucket) {
+          for (int j = 0; j < bucket->len; ++j) {
+              if (!insert_HashMap(m, bucket->block[j])) {
+                //TODO: fix memory leak
+                m->strs = oldStrs;
+                m->size = oldSize;
+                return false;
+            }
+          }
+
+        }
+    }
+    return true;
+}
+
+
+
+bool insert_HashMap(HashMap *m, Str *s) {
+    if (!m) {
+        return false;
+    }
+    double load = (double)m->size / m->len;
+    if (load >= 0.6) {
+        __grow_HashMap(m);
+    }
+    int idx = __hash_Str(s, m->size);
+    StrArray *bucket = m->strs[idx];
+    if (bucket == NULL) {
+        bucket = new_StrArray();
+        if (!bucket)
+            return false;
+    }
+
+    for (int i = 0; i < bucket->len; ++i) {
+        if (strcmp(s->block, bucket->block[i]->block) == 0) {
+            return false;
+        }
+    }
+    if (!append_StrArray(bucket, s)) {
+            return false;
+    }
+    m->len++;
+    return true;
+}
+
+bool has_HashMap(HashMap *m, Str *s) {
+    int idx = __hash_Str(s, m->size);
+    StrArray *bucket = m->strs[idx];
+    if (bucket == NULL) {
+        return false;
+    }
+    for (int i = 0; i < bucket->len; ++i) {
+        if (strcmp(s->block, bucket->block[i]->block) == 0) {
+            return true;
+        }
+    }
+    return false;
+}
+
+void del_HashMap(HashMap *m) {
+    if (!m)
+        return;
+    for(int i = 0; i < m->size; ++i)
+        del_StrArray(m->strs[i]);
+}
