ecfc3faefdc63bc73f35865d8f8d567fe28dd2fb
Banana-4 <stig34768@gmail.com>
2026-04-01 12:42:21 +0000

    add hash map

--- include/memory.h
+++ include/memory.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <stdlib.h>
 #include <stdbool.h>
+#define SIZE 8
 
 typedef struct {
     size_t len;
@@ -20,13 +21,25 @@ typedef struct {
     Str **block;
 } StrArray;
 
+
+typedef struct {
+    size_t len;
+    size_t size;
+    StrArray **strs;
+}HashMap;
+
 Array *new_Array();
 Str *new_Str();
 StrArray *new_StrArray();
+HashMap *new_HashMap();
 
 bool append_Array(Array *a, int n);
 bool append_Str(Str *s, char c);
 bool append_StrArray(StrArray *strs, Str *s);
+
+bool insert_HashMap(HashMap *m, Str *s);
+
 void del_Array(Array *a);
 void del_Str(Str *s);
 void del_StrArray(StrArray *str);
+void del_HashMap(HashMap *m);
