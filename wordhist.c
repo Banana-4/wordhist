f84c210f67fe604087de6ffe186cfd4d31462ff9
Banana-4 <stig34768@gmail.com>
2026-04-03 10:07:59 +0000

    refactor: simplify by removing the lengths array

--- wordhist.c
+++ wordhist.c
@@ -5,26 +5,18 @@
 #include <stdio.h>
 
 int main() {
-    Array *lengths = new_Array();
-    if (!lengths) {
-        return 1;
-    }
     StrArray *words = new_StrArray();
     if (!words) {
-        del_Array(lengths);
         return 1;
     }
-
     int h = 0;
-    int signal = input_loop(words, lengths, &h);
+    int signal = input_loop(words, &h);
     if (signal == 1) {
         fprintf(stderr, "Critical error exiting...\n");
-        del_Array(lengths);
         del_StrArray(words);
         exit(signal);
     }
-    histogram(words, lengths, h);
-    del_Array(lengths);
+    histogram(words, h);
     del_StrArray(words);
     return 0;
 }
