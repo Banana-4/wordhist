ad8e989b19ec635f39ad0d2e6f45fa6a82aa4146
Banana-4 <stig34768@gmail.com>
2026-04-06 08:37:07 +0000

    rework hash map to build str array

--- src/hashmap.c
+++ src/hashmap.c
@@ -10,21 +10,30 @@ HashMap *new_HashMap() {
         return NULL;
     }
     m->size = SIZE;
-    m->strs = (StrArray**)malloc(sizeof(StrArray*) * m->size);
+    m->strs = new_StrArray();
+
     if (!m->strs) {
         free(m);
         return NULL;
     }
+
+    m->idxs = (Array **)malloc(sizeof(Array *) * m->size);
+    if (!m->idxs) {
+        del_StrArray(m->strs);
+        return NULL;
+    }
     for (int i = 0; i < m->size; ++i) {
-        m->strs[i] = NULL;
+        m->idxs[i] = NULL;
     }
     m->len = 0;
     return m;
 }
 
 
+//////////////////////////////
+// AI generated hash function
+/////////////////////////////
 
-//AI generated hash function
 unsigned long __hash(const char *str) {
     unsigned long hash = 5381;
     int c;
@@ -43,33 +52,56 @@ int __hash_Str(Str *s, size_t range) {
 bool __grow_HashMap(HashMap *m) {
     if (!m)
         return false;
-    StrArray **new = (StrArray**)malloc(sizeof(StrArray *) * m->size * 2);
-    if (!new) {
+    size_t new_size = m->size * 2;
+    Array **new_idxs = (Array**)malloc(sizeof(Array *) * new_size);
+    if (!new_idxs) {
         return false;
     }
-    for (int i = 0; i < m->size * 2; ++i)
-        new[i] = NULL;
+    for (int i = 0; i < new_size; ++i)
+        new_idxs[i] = NULL;
 
-    size_t oldSize = m->size;
-    StrArray **oldStrs = m->strs;
-    size_t oldLen = m->len;
-    m->size = m->size * 2;
-    m->strs = new;
-    m->len = 0;
-    for (int i = 0; i < oldSize; ++i) {
-        StrArray *bucket = oldStrs[i];
-        if (bucket != NULL) {
-            for (int j = 0; j < bucket->len; ++j) {
-              if (!insert_HashMap(m, bucket->block[j])) {
-                  m->strs = oldStrs;
-                  m->size = oldSize;
-                  return false;
+    //////////////////////////
+    // Rehashing
+    //////////////////////////
+
+    for (int i = 0; i < m->len; ++i) {
+        int idx = __hash_Str(m->strs->block[i], new_size);
+        if (!new_idxs[idx]) {
+            Array *bucket = new_Array();
+            if (!bucket) {
+               goto grow_cleanup;
             }
-          }
+            new_idxs[idx] = bucket;
+        }
+
+        if (!append_Array(new_idxs[idx], i)) {
+            goto grow_cleanup;
         }
     }
+
+    //////////////////////////
+    // Old memory cleanup
+    /////////////////////////
+
+    for (int i = 0; i < m->size; ++i) {
+        del_Array(m->idxs[i]);
+    }
+    free(m->idxs);
+    m->idxs = new_idxs;
+    m->size = new_size;
     return true;
+
+    //////////////////////////
+    // Error Handling
+    /////////////////////////
+
+    grow_cleanup:
+        for (int i = 0; i < new_size; ++i)
+            del_Array(new_idxs[i]);
+        free(new_idxs);
+        return false;
 }
+
 
 bool insert_HashMap(HashMap *m, Str *s) {
     if (!m) {
@@ -78,49 +110,45 @@ bool insert_HashMap(HashMap *m, Str *s) {
     double load = (double)m->len / m->size;
 
     if (load >= LOAD) {
-        __grow_HashMap(m);
+        if (!__grow_HashMap(m))
+            return false;
     }
+
     int idx = __hash_Str(s, m->size);
-    StrArray *bucket = m->strs[idx];
+    Array *bucket = m->idxs[idx];
 
     if (bucket == NULL) {
-        bucket = new_StrArray();
+        bucket = new_Array();
         if (!bucket)
             return false;
-        m->strs[idx] = bucket;
+        m->idxs[idx] = bucket;
     }
     for (int i = 0; i < bucket->len; ++i) {
-        if (strcmp(s->block, bucket->block[i]->block) == 0) {
+        int idx = bucket->block[i];
+        if (strcmp(s->block, m->strs->block[idx]->block) == 0) {
             return false;
         }
 
     }
-    if (!append_StrArray(bucket, s)) {
+    if (!append_StrArray(m->strs, s)) {
             return false;
+    }
+
+    if (!append_Array(bucket, m->strs->len - 1)) {
+        --m->strs->len;
+        del_Str(m->strs->block[m->len]);
+        return false;
     }
     m->len++;
     return true;
-}
-
-bool has_HashMap(HashMap *m, Str *s) {
-    int idx = __hash_Str(s, m->size);
-    StrArray *bucket = m->strs[idx];
-    if (bucket == NULL) {
-        return false;
-    }
-    for (int i = 0; i < bucket->len; ++i) {
-        if (strcmp(s->block, bucket->block[i]->block) == 0) {
-            return true;
-        }
-    }
-    return false;
 }
 
 void del_HashMap(HashMap *m) {
     if (!m)
         return;
     for (int i = 0; i < m->size; ++i) {
-        free(m->strs[i]);
+        del_Array(m->idxs[i]);
     }
+    free(m->idxs);
     free(m);
 }
