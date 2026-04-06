38627681a04f8b03b328fee17742adc3a5caac4c
Banana-4 <stig34768@gmail.com>
2026-04-06 15:34:28 +0000

    refactor code: extract word storing into a funct

--- src/input.c
+++ src/input.c
@@ -1,4 +1,20 @@
 #include "../include/input.h"
+#include <memory.h>
+
+bool __process_string(HashMap *m, Str *str, int *h) {
+    if(insert_HashMap(m, str)) {
+                if (*h < str->len) {
+                    *h = str->len;
+                }
+                str = NULL;
+                return true;
+            } else {
+                del_Str(str);
+                str = NULL;
+                return false;
+            }
+
+}
 
 int input_loop(StrArray **words, int *h) {
     bool inword = false;
@@ -13,15 +29,7 @@ int input_loop(StrArray **words, int *h) {
     while ((c = getchar()) != EOF) {
         if (inword && (!isalpha(c) && !isdigit(c) && c != '\'')) {
             inword = false;
-            if(insert_HashMap(m, buf)) {
-                if (*h < buf->len) {
-                    *h = buf->len;
-                }
-                buf = NULL;
-            } else {
-                del_Str(buf);
-                buf = NULL;
-            }
+            __process_string(m, buf, h);
         }
 
         else if (inword == false && isalpha(c)) {
@@ -42,16 +50,9 @@ int input_loop(StrArray **words, int *h) {
     }
 
     if (inword) {
-        if(insert_HashMap(m, buf)) {
-            if (*h < buf->len) {
-                    *h = buf->len;
-            }
-            buf = NULL;
-            } else {
-            del_Str(buf);
-            buf = NULL;
-        }
+        __process_string(m, buf, h);
     }
+
     *words = transfer_data(m);
     del_HashMap(m);
     return 0;
