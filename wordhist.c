7371bf3268259ff9daf12b8d8933f9b29fc0a877
Banana-4 <stig34768@gmail.com>
2026-04-07 09:02:23 +0000

    refactor: implement input error codes

--- wordhist.c
+++ wordhist.c
@@ -1,22 +1,21 @@
-#include "include/input.h"
 #include "include/histogram.h"
+#include "include/input.h"
 #include "include/memory.h"
-
 #include <stdio.h>
 
 int main() {
-    StrArray *words = new_StrArray();
-    if (!words) {
-        return 1;
-    }
-    int h = 0;
-    int signal = input_loop(&words, &h);
-    if (signal == 1) {
-        fprintf(stderr, "Critical error exiting...\n");
-        del_StrArray(words);
-        exit(signal);
-    }
-    histogram(words, h);
+  StrArray *words = new_StrArray();
+  if (!words) {
+    return 1;
+  }
+  int h = 0;
+  INP_ERR signal = input_loop(&words, &h);
+  if (signal == INP_MEM_FAIL) {
+    fprintf(stderr, "Critical error exiting...\n");
     del_StrArray(words);
-    return 0;
+    exit(signal);
+  }
+  histogram(words, h);
+  del_StrArray(words);
+  return 0;
 }
