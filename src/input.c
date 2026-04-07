af2f6ed250d60c361a5e83765bf69a559b113b86
Banana-4 <stig34768@gmail.com>
2026-04-07 08:52:42 +0000

    fix: update names

--- src/input.c
+++ src/input.c
@@ -1,8 +1,8 @@
 #include "../include/input.h"
 #include <memory.h>
 
-bool __process_string(StrArrayBuilder *m, Str *str, int *h) {
-    MEM_ERRORS err = insert_StrArrayBuilder(m, str);
+bool __process_string(StrArrayBuilder *builder, Str *str, int *h) {
+    MEM_ERRORS err = insert_StrArrayBuilder(builder, str);
     if(err == NO_ERR) {
                 if (*h < str->len) {
                     *h = str->len;
@@ -22,28 +22,28 @@ int input_loop(StrArray **words, int *h) {
     *h = 0;
     Str *buf = NULL;
     int c;
-    StrArrayBuilder *m = new_HashMap();
-    if (!m) {
+    StrArrayBuilder *builder = new_StrArrayBuilder();
+    if (!builder) {
         return 1;
     }
 
     while ((c = getchar()) != EOF) {
         if (inword && (!isalpha(c) && !isdigit(c) && c != '\'')) {
             inword = false;
-            __process_string(m, buf, h);
+            __process_string(builder, buf, h);
         }
 
         else if (inword == false && isalpha(c)) {
             inword = true;
             buf = new_Str();
             if (!buf) {
-                del_StrArrayBuilder(m);
+                del_StrArrayBuilder(builder);
                 return 1;
             }
         }
         if (inword) {
             if (!append_Str(buf, c)) {
-                del_StrArrayBuilder(m);
+                del_StrArrayBuilder(builder);
                 del_Str(buf);
                 return 1;
             }
@@ -51,10 +51,10 @@ int input_loop(StrArray **words, int *h) {
     }
 
     if (inword) {
-        __process_string(m, buf, h);
+        __process_string(builder, buf, h);
     }
 
-    *words = transfer_data(m);
-    del_StrArrayBuilder(m);
+    *words = transfer_data(builder);
+    del_StrArrayBuilder(builder);
     return 0;
 }
