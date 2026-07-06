4b21467599547213144a3b08a7365a73fca505a4
Banana-4 <stig34768@gmail.com>
2026-07-06 10:19:35 +0000

    Add word histogram wrapper scrtipt with terminal-aware layout
    This script improves UX by validating the file before execution and  automatically selecting the histogram layout. if the  number of words is greater than the terminal width, it runs 'wordhist'  in horizontal mode else it uses the vertical layout.

---
+++ wordhist.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <filename>"
+    exit 1
+fi
+
+filename="$1"
+
+if [ ! -f "$filename" ]; then
+    echo "Error: '$filename' does not exist or is not a regular file."
+    exit 1
+fi
+
+count=$(grep -oE "[[:alpha:]][[:alnum:]']*" "$filename" | wc -l)
+cols=$(tput cols)
+if [ "$count" -gt "$cols" ]; then
+	./wordhist h < "$filename"
+else
+	./wordhist < "$filename"
+fi
