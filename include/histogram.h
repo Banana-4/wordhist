5e0bf37530544f550068caf13d98b762ce297547
Banana-4 <stig34768@gmail.com>
2026-07-06 10:12:53 +0000

    refactor: add a flag for horizontal histogram

--- include/histogram.h
+++ include/histogram.h
@@ -1,5 +1,8 @@
 #pragma once
 #include "../include/memory.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 
-void histogram(StrArray *words, int h);
+void histogram(StrArray *words, int h, bool horizontal);
