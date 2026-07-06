6a7ca247dc4b28c54487386bf3bc2e02c7e2077f
Banana-4 <stig34768@gmail.com>
2026-07-06 10:13:49 +0000

    refactor: improve UX
    Add a option to draw horizontal histogram

--- wordhist.c
+++ wordhist.c
@@ -2,21 +2,28 @@
 #include "include/histogram.h"
 #include "include/input.h"
 #include "include/memory.h"
-#include <stdio.h>
 
-int main() {
-  StrArray *words = new_StrArray();
-  if (!words) {
-    return 1;
-  }
-  int h = 0;
-  ERROR_CODES signal = input_loop(&words, &h);
-  if (signal == ALLOCATION_FAIL) {
-    fprintf(stderr, "Critical error exiting...\n");
+int main(int argc, char* argv[]) {
+	bool horizontal = false;
+	if (argc == 2) {
+  		if (argv[1][0] == 'h'){
+  			horizontal = true;
+    	} else {
+     		fprintf(stderr, "Usage: ./wordhist (h) \n");
+     	}
+  	}
+  	StrArray *words = new_StrArray();
+   	if (!words) {
+    	return 1;
+    }
+   	int h = 0;
+   	ERROR_CODES signal = input_loop(&words, &h);
+   	if (signal == ALLOCATION_FAIL) {
+    	fprintf(stderr, "Critical error exiting...\n");
+     	del_StrArray(words);
+      exit(signal);
+    }
+    histogram(words, h, horizontal);
     del_StrArray(words);
-    exit(signal);
-  }
-  histogram(words, h);
-  del_StrArray(words);
-  return 0;
+    return 0;
 }
